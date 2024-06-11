0423ce86ff59773eb952f961e4f52526df8f9b4e
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-06-11 13:28:20 +0300

    ....

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,13 @@
+import json
 from apscheduler.schedulers.background import BackgroundScheduler
+from sqlalchemy import Integer, cast
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime, timedelta
 from DataBase import SessionLocal
 import models
 import requests
+from google.oauth2 import service_account
+import google.auth.transport.requests
 
 def delete_last_year_appointments():
     current_year = datetime.now().year
@@ -22,39 +26,55 @@ def send_notifications():
         session = SessionLocal()
         now = datetime.now()
         formatted_now = now.strftime("%Y-%m-%d")
+        print(f"formatted date {formatted_now}")
         two_hours_from_now = now + timedelta(hours=2)
         formatted_hour = two_hours_from_now.hour
+        print(f"formatted hour {formatted_hour}")
+
         appointments = session.query(models.Appointment).filter(
-            models.Appointment.appointmentDate >= formatted_now,
-            models.Appointment.From <= formatted_hour
+            models.Appointment.appointmentDate == formatted_now,
+            cast(models.Appointment.fromTime, Integer) <= formatted_hour
         ).all()
         
+        if not appointments:
+            print("No appointments found")
+            session.close()
+            return
+        
         for appointment in appointments:
-            fcnTokens = session.query(models.fcm).filter(models.fcm.userId == appointment.parentId).all()
-            # Send notification for each appointment
-            send_notification(appointment, fcnTokens)
+            fcms = session.query(models.fcm).filter(models.fcm.userId == appointment.parentId).all()
+            send_notification(appointment, fcms)
         
         session.close()
     except SQLAlchemyError as e:
         print(f"An error occurred while querying appointments: {e}")
 
-def send_notification(appointment, fcmTokens):
-    # Add your notification logic here
-    # Example: Sending a notification to the user associated with the appointment
-    for fcm_token in fcmTokens:
-        message = f"You have an appointment scheduled at {appointment.appointmentDate} from {appointment.From} to {appointment.To}"
-        api_key = 'AIzaSyAggWWiMWAbKw834_37RWzz391j2VNHVmU'  # Replace with your actual API key (current_key)
+def send_notification(appointment, fcms):
+    # Define the required scope for FCM
+    scopes = ['https://www.googleapis.com/auth/firebase.messaging']
+    # Load the credentials from the service account key file with the defined scopes
+    credentials = service_account.Credentials.from_service_account_file('firebase_key.json', scopes=scopes)
+    auth_request = google.auth.transport.requests.Request()
+    credentials.refresh(auth_request)
+    access_token = credentials.token
+
+    for fcm in fcms:
+        token = fcm.fcmToken
+        message = f"You have an appointment scheduled on {appointment.appointmentDate} from {appointment.fromTime} to {appointment.toTime}"
         headers = {
             'Content-Type': 'application/json',
+            'Authorization': f'Bearer {access_token}'
         }
         payload = {
-            'notification': {
-                'title': 'Appointment Reminder',
-                'body': message,
-            },
-            'to': fcm_token,
+            'message': {
+                'notification': {
+                    'title': 'Appointment Reminder',
+                    'body': message,
+                },
+                'token': token,
+            }
         }
-        response = requests.post('https://fcm.googleapis.com/fcm/send', headers=headers, params={'key': api_key}, json=payload)
+        response = requests.post('https://fcm.googleapis.com/v1/projects/pulse-pediatric/messages:send', headers=headers, json=payload)
         if response.status_code == 200:
             print("Notification sent successfully")
         else:
@@ -64,5 +84,6 @@ def send_notification(appointment, fcmTokens):
 def start_scheduler():
     scheduler = BackgroundScheduler()
     scheduler.add_job(delete_last_year_appointments, 'cron', year='*', month='1', day='1', hour='0', minute='0')
-    scheduler.add_job(send_notifications, 'cron', minute='*')  # Run every minute
+    scheduler.add_job(send_notifications, 'cron', minute='*')
     scheduler.start()
+
