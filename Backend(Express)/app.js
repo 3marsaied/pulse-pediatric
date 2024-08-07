841dbd067d4a8a995d0775556e2b26fca6751c80
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-08-08 01:16:42 +0300

    v

--- Backend(Express)/app.js
+++ Backend(Express)/app.js
@@ -10,12 +10,13 @@ const port = 3001;
 
 app.use(express.json());
 
-app.use(cors({
-  origin: '*',
-  methods: '*',
-  allowedHeaders: '*',
+
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,
-}));
+};
 
 const mongoURI = process.env.MONGOURI;
 
